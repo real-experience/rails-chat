22d5c56f9d99928302df3dc9095897c1357ed08d
nishant-ranjan28 <nishantranjan78@gmail.com>
2020-05-20 16:43:00 +0530

    send message module

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -3,11 +3,11 @@ class RoomMessagesController < ApplicationController
 
   def create
     @room_message = RoomMessage.create user: current_user,
-                                     room: @room,
-                                     message: params.dig(:room_message, :message)
+                                       room: @room,
+                                       message: params.dig(:room_message, :message)
 
     redirect_to room_path @room
-  # RoomChannel.broadcast_to @room, @room_message
+    # RoomChannel.broadcast_to @room, @room_message
   end
 
   protected
